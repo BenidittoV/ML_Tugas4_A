e5da16feb7700191922c2890d5b76c044ca43fa0
Beniditto Eka Viyantyo <134614584+BenidittoV@users.noreply.github.com>
2024-11-07 16:03:54 +0700

    Update Tugas_Modul4_A_Ditto_11605.py

--- Tugas_Modul4_A_Ditto_11605.py
+++ Tugas_Modul4_A_Ditto_11605.py
@@ -1,28 +1,31 @@
-import os
-import pickle
-
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
-from sklearn.metrics import pairwise_distances
-
+import pandas as pd
+import pickle 
+import os
+import plotly.express as px 
+import numpy as np
+from sklearn.metrics import pairwise_distances 
+import plotly.graph_objects as go 
 
 def scatter(model, model_name, data, new_point, features, color_scale, title):
     clusters = model.fit_predict(data[features]) #untuk memprediksi cluster untuk setiap title pada data menggunakan model
     data[f'{model_name}_Cluster'] = clusters
     
+    #menentukan cluster untuk titik baru
     if model_name == "KMeans_model":
+        #pada k-means, dilakukan predict langsung
         new_cluster = model.predict(new_point[features])[0]
     else:
+        #pada agglomeratv dan dbscan dilakukan dengan cara menghitung jarak titik terdekat
         distances = pairwise_distances(new_point[features], data[features])
         nearest_index = distances.argmin()
         new_cluster = clusters[nearest_index]
     
+    #membuat grafik 3D menggunakan Plotly Express
     fig = px.scatter_3d(data, x='Avg_Credit_Limit', y='Total_Credit_Cards', z='Total_visits_online',
                        color=f'{model_name}_Cluster', title=title, color_continuous_scale=color_scale)
     
+    #menambahkan titik baru pada grafik
     fig.add_trace(
         go.Scatter3d(
             x=new_point['Avg_Credit_Limit'],
@@ -56,6 +59,7 @@ if uploaded_file is not None:
         'DBSCAN_model' : os.path.join(r'DBSCAN_model.pkl')
     }
     
+    #load model ketiga model ke dalam dictionary
     models = {}
     
     for model_name, path in model_path.items():
@@ -65,12 +69,15 @@ if uploaded_file is not None:
         else:
             st.write(f"Model {model_name} tidak ditemukan di path : ", path)
             
+    #sidebar untuk memasukkan (menginputkan) nilai untuk titik baru yang akan diprediksi clusternya
     avg_CL = st.sidebar.number_input("Average Credit Limit", 0, 100000)
     sum_CC = st.sidebar.number_input("Total Credit Cards", 0, 10)
     sum_VO = st.sidebar.number_input("Total Visits Online", 0, 10)
     
     if st.sidebar.button("Prediksi !"):
+        #fitur yang digunakan untuk memprediksi
         features = ['Avg_Credit_Limit', 'Total_Credit_Cards', 'Total_visits_online']
+        #memasukkan data titik baru ke dalam DataFrame
         new_point = pd.DataFrame({
             'Avg_Credit_Limit': [avg_CL],
             'Total_Credit_Cards': [sum_CC],
